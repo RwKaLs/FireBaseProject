e50b01c6314ac2c2a85a45611d2a30e1e6ea9b30
RwKaLs <53008072+RwKaLs@users.noreply.github.com>
2021-05-07 15:48:20 +0300

    1st commit

---
+++ app/src/main/java/ru/myitschool/vsu2020/firebase/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.myitschool.vsu2020.firebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
